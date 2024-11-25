ee3a583bf6d7013dc4df453a126d67acec31ea73
Aaron Wald <aaronwald@gmail.com>
2024-11-25 00:06:18 +0000

    add HTML

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '''
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Hello, World!</title>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Hello, World!</h1>
+          <p>This is a simple HTML page served by Flask.</p>
+        </div>
+      </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
